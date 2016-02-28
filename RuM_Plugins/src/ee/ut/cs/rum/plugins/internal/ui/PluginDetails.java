bb8b0cc68ebb843333fa70c3cea9ee89b0b1dfaa
FableBlaze <oolop7@hotmail.com>
2016-02-28 20:21:20 +0200

    Removed unnecessary imports

--- RuM_Plugins/src/ee/ut/cs/rum/plugins/internal/ui/PluginDetails.java
+++ RuM_Plugins/src/ee/ut/cs/rum/plugins/internal/ui/PluginDetails.java
@@ -1,9 +1,6 @@
 package ee.ut.cs.rum.plugins.internal.ui;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CTabFolder;
-import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
