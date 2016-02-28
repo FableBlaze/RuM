ea63495bbf44fb59c4b099472e1911e8d93f1d66
FableBlaze <oolop7@hotmail.com>
2016-02-28 21:51:56 +0200

    Removed old experimenting code from RumUI

--- RuM/src/ee/ut/cs/rum/RumUI.java
+++ RuM/src/ee/ut/cs/rum/RumUI.java
@@ -7,45 +7,9 @@ import ee.ut.cs.rum.plugins.ui.PluginsManagementUI;
 public class RumUI extends AbstractEntryPoint {
 	private static final long serialVersionUID = 1282027955721012064L;
 
-//	private String buttonText = "+1";
-
 	public void createContents( Composite parent ) {
-		
-//		Button button = new Button(parent, SWT.PUSH);
-//		button.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false));
-//		button.setText(buttonText);
-//
-//		final Label label = new Label(parent, SWT.NONE);
-//		label.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false));
-//		label.setText("No event yet.");
-//
-//		button.addListener(SWT.Selection, new Listener() {
-//			private static final long serialVersionUID = 5362888762173659283L;
-//			int counter = 0;
-//
-//			@Override
-//			public void handleEvent(Event arg0) {
-//				label.setText("Clicked" + " [" + ++counter + "]");
-//				Activator.getLogger().info(button.getText() + " was pressed, counter is " + counter);
-//
-//				Query query = em.createQuery("Select p from Plugin p order by p.id");
-//				List<Plugin> plugins = query.getResultList();
-//
-//				plugins.get(0).setDescription("blah-blah-blah" + counter);
-//				em.getTransaction().begin();
-//				em.persist(plugins.get(0));
-//				em.getTransaction().commit();
-//				em.close();
-//			}
-//		});
-		
-//		PluginsManagementUI pluginsManagementUI = new PluginsManagementUI(parent);
-
 		new PluginsManagementUI(parent);
 		Activator.getLogger().info("Someone opened ee.ut.cs.rum.RumUI");
 	}
 
-//	public void setButtonText(String buttonText) {
-//		this.buttonText = buttonText;
-//	}
 }
