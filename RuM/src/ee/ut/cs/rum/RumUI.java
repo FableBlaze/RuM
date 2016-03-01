34cbe12d605c2c3c1d9c6fa95867b73f66eabb97
FableBlaze <oolop7@hotmail.com>
2016-03-02 00:41:25 +0200

    Switched from using hidden Shell to using StackLayout for navigation

--- RuM/src/ee/ut/cs/rum/RumUI.java
+++ RuM/src/ee/ut/cs/rum/RumUI.java
@@ -2,58 +2,65 @@ package ee.ut.cs.rum;
 
 import org.eclipse.rap.rwt.application.AbstractEntryPoint;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StackLayout;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
 
 import ee.ut.cs.rum.plugins.ui.PluginsManagementUI;
 
 public class RumUI extends AbstractEntryPoint {
 	private static final long serialVersionUID = 1282027955721012064L;
-	
-	private Composite visibleComponent;
-    private Shell hiddenComponentsShell;
 
-	public void createContents( Composite parent ) {
-		hiddenComponentsShell = new Shell();
+	public void createContents(Composite parent) {
+		Composite buttonArea = new Composite(parent, SWT.NONE);
+		buttonArea.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+		buttonArea.setLayout(new GridLayout(2, false));
+
+		Button workspaceButton = new Button(buttonArea, SWT.PUSH);
+		workspaceButton.setText("Workspace");
+
+		Button pluginsManagemenButton = new Button(buttonArea, SWT.PUSH);
+		pluginsManagemenButton.setText("Plugins management");
+
+		Composite container = new Composite(parent, SWT.NONE);
+		container.setLayoutData( new GridData(SWT.FILL, SWT.FILL, true, true));
+		StackLayout stackLayout = new StackLayout();
+		container.setLayout(stackLayout);
+
+		Composite workspaceSection = new Composite(container, SWT.BORDER);
+		workspaceSection.setLayout(new GridLayout(2, false));
+		Label l = new Label(workspaceSection, SWT.NONE);
+		l.setText("Workspace contents (TODO)");
 		
-		Button workspacesButton = new Button(parent, SWT.PUSH);
-		Button pluginsManagemenButton = new Button(parent, SWT.PUSH);
+		Composite pluginsManagementSection = new PluginsManagementUI(container);
 		
-		Composite workspacesUI = new Composite(hiddenComponentsShell, SWT.BORDER);
-		workspacesUI.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
-		Composite pluginsManagementUI = new PluginsManagementUI(parent);
-		visibleComponent=pluginsManagementUI;
-		
-		
-        workspacesButton.setText("Workspaces");
-        workspacesButton.addSelectionListener(new SelectionAdapter() {
-            public void widgetSelected(SelectionEvent event) {
-                if (visibleComponent!=workspacesUI) {
-                	visibleComponent.setParent(hiddenComponentsShell);
-                    workspacesUI.setParent(parent);
-                    visibleComponent=workspacesUI;
-                    parent.layout();
-                }
-            }
-        });
-		
-        pluginsManagemenButton.setText("Plugins management");
-        pluginsManagemenButton.addSelectionListener(new SelectionAdapter() {
-        	public void widgetSelected(SelectionEvent event) {
-        		if (visibleComponent!=pluginsManagementUI) {
-        			visibleComponent.setParent(hiddenComponentsShell);
-        			pluginsManagementUI.setParent(parent);
-                    visibleComponent=pluginsManagementUI;
-                    parent.layout();
-        		}
-        	}
-        });
-		
+		stackLayout.topControl = workspaceSection;
+		container.layout();
+
+
+		workspaceButton.addSelectionListener( new SelectionAdapter() {
+			private static final long serialVersionUID = -7714989063610717365L;
+
+			public void widgetSelected( SelectionEvent event ) {
+				stackLayout.topControl = workspaceSection;
+				container.layout();
+			}
+		});
+
+		pluginsManagemenButton.addSelectionListener( new SelectionAdapter() {
+			private static final long serialVersionUID = 307591838016942982L;
+
+			public void widgetSelected( SelectionEvent event ) {
+				stackLayout.topControl = pluginsManagementSection;
+				container.layout();
+			}
+		});
+
 		Activator.getLogger().info("Someone opened ee.ut.cs.rum.RumUI");
 	}
 
