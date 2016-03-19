cff7b0bdc6f4f306e8fabd9e67f7ddb680208cf4
FableBlaze <oolop7@hotmail.com>
2016-03-19 23:38:06 +0200

    Updated ToyPlugin structure

--- ToyPlugin1/src/ee/ut/cs/rum/example/plugin/v1/ToyPlugin1.java
+++
@@ -1,5 +0,0 @@
-package ee.ut.cs.rum.example.plugin.v1;
-
-public class ToyPlugin1 {
-
-}
---
+++ ToyPlugin1/src/ee/ut/cs/rum/example/plugin/v1/factory/RumPluginConfigurationImpl.java
@@ -0,0 +1,23 @@
+package ee.ut.cs.rum.example.plugin.v1.factory;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+import ee.ut.cs.rum.plugins.interfaces.factory.RumPluginConfiguration;
+
+public class RumPluginConfigurationImpl implements RumPluginConfiguration {
+
+	@Override
+	public void createConfigurationUi(Composite parent) {
+		Label l = new Label(parent, SWT.BORDER);
+		l.setText("TODO");
+	}
+
+	@Override
+	public Object getConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
