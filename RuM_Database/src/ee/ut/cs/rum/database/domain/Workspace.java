1f89511cfb4e9362ad0f57d11c8fafc0de963565
FableBlaze <oolop7@hotmail.com>
2016-03-20 22:49:36 +0200

    domain workspace

---
+++ RuM_Database/src/ee/ut/cs/rum/database/domain/Workspace.java
@@ -0,0 +1,42 @@
+package ee.ut.cs.rum.database.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="workspace")
+public class Workspace {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "description")
+	private String description;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Workspace [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+}
