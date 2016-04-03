58aad6711c598c051a0d538d0b4c052a69e0eea2
FableBlaze <oolop7@hotmail.com>
2016-04-03 21:58:05 +0300

    Get plugin info from database instead of loading the plugin

--- RuM_Workspace/src/ee/ut/cs/rum/workspaces/internal/ui/task/newtask/pluginstable/PluginSelectionChangedListener.java
+++ RuM_Workspace/src/ee/ut/cs/rum/workspaces/internal/ui/task/newtask/pluginstable/PluginSelectionChangedListener.java
@@ -5,19 +5,13 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleException;
-import org.osgi.framework.ServiceReference;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import ee.ut.cs.rum.database.domain.Plugin;
 import ee.ut.cs.rum.plugins.development.description.PluginInfo;
 import ee.ut.cs.rum.plugins.development.description.deserializer.PluginInfoDeserializer;
-import ee.ut.cs.rum.plugins.development.interfaces.RumPluginFactory;
 import ee.ut.cs.rum.plugins.development.ui.PluginConfigurationUi;
-import ee.ut.cs.rum.workspaces.internal.Activator;
 import ee.ut.cs.rum.workspaces.internal.ui.task.newtask.NewTaskDetails;
 
 public class PluginSelectionChangedListener implements ISelectionChangedListener {
@@ -31,81 +25,25 @@ public class PluginSelectionChangedListener implements ISelectionChangedListener
 	public void selectionChanged(final SelectionChangedEvent event) {
 		IStructuredSelection selection = (IStructuredSelection)event.getSelection();
 		Plugin selectedPlugin = (Plugin) selection.getFirstElement();
-		Bundle selectedPluginBundle = null;
 
 		if (selectedPlugin!=null) {
-			selectedPluginBundle = findSelectedPluginBundle(selectedPlugin);
-			if (selectedPluginBundle==null) {
-				selectedPluginBundle = installSelectedPluginBundle(selectedPlugin);
+			newTaskDetails.getSelectedPluginInfo().updateSelectedPluginInfo(selectedPlugin);
+
+			ScrolledComposite selectedPluginConfigurationUi = newTaskDetails.getSelectedPluginConfigurationUi();
+
+			if (selectedPluginConfigurationUi.getContent()!=null && !selectedPluginConfigurationUi.getContent().isDisposed()) {
+				selectedPluginConfigurationUi.getContent().dispose();
 			}
-		}
-		
-		if (selectedPluginBundle==null) {
-			newTaskDetails.getFooterButtonsComposite().getStartButton().setEnabled(false);
-		} else {
-			newTaskDetails.getFooterButtonsComposite().getStartButton().setEnabled(true);
-		}
 
-		newTaskDetails.getSelectedPluginInfo().updateSelectedPluginInfo(selectedPlugin);
-		updateNewTaskPluginConfigurationUi(selectedPluginBundle);
-	}
+			GsonBuilder gsonBuilder = new GsonBuilder();
+			gsonBuilder.registerTypeAdapter(PluginInfo.class, new PluginInfoDeserializer());
+			Gson gson = gsonBuilder.create();
+			PluginInfo pluginInfo = gson.fromJson(selectedPlugin.getPluginInfo(), PluginInfo.class);
 
-
-	private Bundle findSelectedPluginBundle(Plugin selectedPlugin) {
-		for (Bundle bundle : Activator.getContext().getBundles()) {
-			if (bundle.getLocation().equals("file:///" + selectedPlugin.getFileLocation())) {
-				return bundle;
-			}
-		}
-		return null;
-	}
-
-	private Bundle installSelectedPluginBundle(Plugin selectedPlugin) {
-		Bundle selectedPluginBundle = null;
-		try {
-			selectedPluginBundle = Activator.getContext().installBundle("file:///" + selectedPlugin.getFileLocation());
-			selectedPluginBundle.start();
-		} catch (BundleException e) {
-			Activator.getLogger().info("Failed loading plugin: " + selectedPlugin.toString());
-		}
-		return selectedPluginBundle;
-	}
-
-
-	private void updateNewTaskPluginConfigurationUi(Bundle selectedPluginBundle) {
-		ScrolledComposite selectedPluginConfigurationUi = newTaskDetails.getSelectedPluginConfigurationUi();
-
-		if (selectedPluginConfigurationUi.getContent()!=null && !selectedPluginConfigurationUi.getContent().isDisposed()) {
-			selectedPluginConfigurationUi.getContent().dispose();
-		}
-
-		if (selectedPluginBundle!=null && selectedPluginBundle.getRegisteredServices()!=null) {
-			for (ServiceReference<?> serviceReference : selectedPluginBundle.getRegisteredServices()) {
-				if (implementsRumPluginFactory(serviceReference)) {
-					RumPluginFactory rumPluginFactory = (RumPluginFactory) selectedPluginBundle.getBundleContext().getService(serviceReference);
-					String pluginInfoJson = rumPluginFactory.getPluginInfoJSON();
-					
-					GsonBuilder gsonBuilder = new GsonBuilder();
-					gsonBuilder.registerTypeAdapter(PluginInfo.class, new PluginInfoDeserializer());
-					Gson gson = gsonBuilder.create();
-					PluginInfo pluginInfo = gson.fromJson(pluginInfoJson, PluginInfo.class);
-					
-					PluginConfigurationUi pluginConfigurationUi = new PluginConfigurationUi(selectedPluginConfigurationUi, pluginInfo);
-					selectedPluginConfigurationUi.setContent(pluginConfigurationUi);
-					selectedPluginConfigurationUi.setSize(selectedPluginConfigurationUi.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-					selectedPluginConfigurationUi.getParent().setSize(selectedPluginConfigurationUi.getParent().computeSize(SWT.DEFAULT, SWT.DEFAULT));
-				}
-			}
-		}
-	}
-
-	private boolean implementsRumPluginFactory(ServiceReference<?> serviceReference) {
-		String[] objectClasses = (String[])serviceReference.getProperty("objectClass");
-		for (String objectClass : objectClasses) {
-			if (objectClass.equals(RumPluginFactory.class.getName())) {
-				return true;
-			}
-		}
-		return false;
+			PluginConfigurationUi pluginConfigurationUi = new PluginConfigurationUi(selectedPluginConfigurationUi, pluginInfo);
+			selectedPluginConfigurationUi.setContent(pluginConfigurationUi);
+			selectedPluginConfigurationUi.setSize(selectedPluginConfigurationUi.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+			selectedPluginConfigurationUi.getParent().setSize(selectedPluginConfigurationUi.getParent().computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		}		
 	}
 }
