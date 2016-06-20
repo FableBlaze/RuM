c06eb36f3298f1dde1c35ffa588ffad2a7b233ce
FableBlaze <oolop7@hotmail.com>
2016-06-20 22:44:59 +0300

    Placeholder for redesigned NewTaskDetails

---
+++ RuM_Workspace/src/ee/ut/cs/rum/workspace/internal/ui/task/newtask/NewTaskDetails.java
@@ -0,0 +1,17 @@
+package ee.ut.cs.rum.workspace.internal.ui.task.newtask;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+import ee.ut.cs.rum.workspace.internal.ui.project.ProjectTabFolder;
+
+public class NewTaskDetails extends Composite {
+	private static final long serialVersionUID = -4167600812621979994L;
+
+	NewTaskDetails(ProjectTabFolder projectTabFolder) {
+		super(projectTabFolder, SWT.NONE);
+		
+		
+	}
+
+}
