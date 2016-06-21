3249f6e9ec6be2092ed38ef7b7bc151a362588b4
FableBlaze <oolop7@hotmail.com>
2016-06-21 18:44:05 +0300

    Task and newTask details sidebar will e separate

--- RuM_Workspace/src/ee/ut/cs/rum/workspace/internal/ui/task/newtask/DetailsSideBar.java
+++ RuM_Workspace/src/ee/ut/cs/rum/workspace/internal/ui/task/newtask/DetailsSideBar.java
@@ -1,4 +1,4 @@
-package ee.ut.cs.rum.workspace.internal.ui.task.details;
+package ee.ut.cs.rum.workspace.internal.ui.task.newtask;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -6,8 +6,9 @@ import org.eclipse.swt.widgets.Composite;
 public class DetailsSideBar extends Composite {
 	private static final long serialVersionUID = 17721630002679216L;
 
-	DetailsSideBar(TaskDetails taskDetails) {
+	DetailsSideBar(NewTaskDetails taskDetails) {
 		super(taskDetails, SWT.NONE);
 		
 	}
+
 }
