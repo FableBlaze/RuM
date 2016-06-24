b4fbd354b05affad48f1bff1f4549344bdb73dee
FableBlaze <oolop7@hotmail.com>
2016-06-24 20:31:52 +0300

    Removed unnecessary fields from domain Task

--- RuM_Database/src/ee/ut/cs/rum/database/domain/Task.java
+++ RuM_Database/src/ee/ut/cs/rum/database/domain/Task.java
@@ -27,14 +27,8 @@ public class Task {
 	private String description;
 	@Column(name = "status")
 	private TaskStatus status;
-	@JoinColumn(name = "plugin_fk")
-	private Plugin plugin;
-	@Column(name = "configuration_values", columnDefinition = "TEXT")
-	private String configurationValues;
 	@JoinColumn(name = "project_fk")
 	private Project project;
-	@Column(name = "output_path")
-	private String outputPath;
 	@Column(name = "created_by")
 	private String createdBy;
 	@Column(name = "created_at")
@@ -67,29 +61,11 @@ public class Task {
 	public void setStatus(TaskStatus status) {
 		this.status = status;
 	}
-	public Plugin getPlugin() {
-		return plugin;
-	}
-	public void setPlugin(Plugin plugin) {
-		this.plugin = plugin;
-	}
-	public String getConfigurationValues() {
-		return configurationValues;
-	}
-	public void setConfigurationValues(String configurationValues) {
-		this.configurationValues = configurationValues;
-	}
 	public Project getProject() {
 		return project;
 	}
 	public void setProject(Project project) {
 		this.project = project;
-	}
-	public String getOutputPath() {
-		return outputPath;
-	}
-	public void setOutputPath(String outputPath) {
-		this.outputPath = outputPath;
 	}
 	public String getCreatedBy() {
 		return createdBy;
@@ -118,9 +94,8 @@ public class Task {
 	
 	@Override
 	public String toString() {
-		return "Task [id=" + id + ", name=" + name + ", description=" + description + ", status=" + status + ", plugin="
-				+ plugin + ", configurationValues=" + configurationValues + ", project=" + project + ", outputPath="
-				+ outputPath + ", createdBy=" + createdBy + ", createdAt=" + createdAt + ", lastModifiedBy="
+		return "Task [id=" + id + ", name=" + name + ", description=" + description + ", status=" + status
+				+ ", project=" + project + ", createdBy=" + createdBy + ", createdAt=" + createdAt + ", lastModifiedBy="
 				+ lastModifiedBy + ", lastModifiedAt=" + lastModifiedAt + "]";
-	}
+		}
 }
