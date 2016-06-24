84555dc3ad0764b3437abf8697e5aad9e1101dde
FableBlaze <oolop7@hotmail.com>
2016-06-24 22:28:00 +0300

    PluginsOverview is now updatable by controller

--- RuM_Plugins/src/ee/ut/cs/rum/plugins/internal/ui/overview/PluginsOverview.java
+++ RuM_Plugins/src/ee/ut/cs/rum/plugins/internal/ui/overview/PluginsOverview.java
@@ -11,20 +11,27 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 
 import ee.ut.cs.rum.controller.RumController;
+import ee.ut.cs.rum.database.domain.Plugin;
 import ee.ut.cs.rum.database.util.PluginAccess;
+import ee.ut.cs.rum.enums.ControllerEntityType;
+import ee.ut.cs.rum.enums.ControllerUpdateType;
+import ee.ut.cs.rum.interfaces.RumUpdatableView;
 import ee.ut.cs.rum.plugins.internal.ui.dialog.PluginUploadDialog;
 
-public class PluginsOverview extends Composite {
+public class PluginsOverview extends Composite implements RumUpdatableView {
 	private static final long serialVersionUID = 6363000997779117721L;
-	
+
+	private Display display;
 	private RumController rumController;
 	
-	private Label numberOfPluginsLable;
+	private Label totalPlugins;
 
 	public PluginsOverview(OverviewTabContents overviewTabContents, RumController rumController) {
 		super(overviewTabContents, SWT.NONE);
 		
+		this.display=Display.getCurrent();
 		this.rumController=rumController;
+		rumController.registerView(this, ControllerEntityType.PLUGIN);
 		
 		this.setLayoutData(new GridData(SWT.LEFT, SWT.FILL, false, true));
 		this.setLayout(new GridLayout(2, false));
@@ -38,10 +45,9 @@ public class PluginsOverview extends Composite {
 		Label label = new Label(this, SWT.NONE);
 		label.setText("Total plugins:");
 		label.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, true));
-		
-		numberOfPluginsLable = new Label(this, SWT.NONE);
-		numberOfPluginsLable.setText(Integer.toString(PluginAccess.getPluginsDataFromDb().size()));
-		numberOfPluginsLable.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, true));
+		totalPlugins = new Label(this, SWT.NONE);
+		totalPlugins.setText(Integer.toString(PluginAccess.getPluginsDataFromDb().size()));
+		totalPlugins.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, true));
 		
 		Button addPluginDialogueButton = new Button(this, SWT.PUSH);
 		addPluginDialogueButton.setText("Add plugin");
@@ -57,5 +63,33 @@ public class PluginsOverview extends Composite {
 				pluginUploadDialog.open();
 			}
 		});
-	}	
+	}
+
+	@Override
+	public void controllerUpdateNotify(ControllerUpdateType updateType, Object updatedEntity) {
+		if (updatedEntity instanceof Plugin) {
+			display.asyncExec(new Runnable() {
+				public void run() {
+					int totalPluginsCount = Integer.parseInt(totalPlugins.getText());
+					switch (updateType) {
+					case CREATE:
+						totalPluginsCount+=1;
+						break;
+					case DELETE:
+						totalPluginsCount-=1;
+						break;
+					default:
+						break;
+					}
+					totalPlugins.setText(Integer.toString(totalPluginsCount));							
+				}
+			});
+		}
+	}
+
+	@Override
+	public void dispose() {
+		rumController.unregisterView(this, ControllerEntityType.PLUGIN);
+		super.dispose();
+	}
 }
