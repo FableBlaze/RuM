92c036c11f985a57084844cab5e556704c1e1c2e
FableBlaze <oolop7@hotmail.com>
2016-07-15 21:33:10 +0300

    Update userFiles list if userFile data is changed trough controller

--- RuM_Workspace/src/ee/ut/cs/rum/workspace/internal/ui/task/newtask/NewTaskDetailsContainer.java
+++ RuM_Workspace/src/ee/ut/cs/rum/workspace/internal/ui/task/newtask/NewTaskDetailsContainer.java
@@ -18,41 +18,41 @@ import ee.ut.cs.rum.workspace.internal.Activator;
 
 public class NewTaskDetailsContainer extends Composite implements RumUpdatableView {
 	private static final long serialVersionUID = -7982581022298012511L;
-	
+
 	private RumController rumController;
-		
+
 	private NewTaskComposite newTaskComposite;
 	private NewTaskGeneralInfo newTaskGeneralInfo;
-	
+
 	private List<UserFile> userFiles;
 	private List<NewTaskSubTaskInfo> newTaskSubTaskInfoList;
 
 	public NewTaskDetailsContainer(NewTaskComposite newTaskComposite, RumController rumController) {
 		super(newTaskComposite, SWT.NONE);
-		
+
 		this.rumController=rumController;
 		rumController.registerView(this, ControllerEntityType.USER_FILE);
-		
+
 		this.newTaskComposite=newTaskComposite;
 		this.userFiles = UserFileAccess.getProjectUserFilesDataFromDb(newTaskComposite.getProjectTabFolder().getProject().getId());
-		
+
 		this.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		this.setLayout(new StackLayout());
-		
+
 		this.newTaskGeneralInfo = new NewTaskGeneralInfo(this);
-		
+
 		this.newTaskSubTaskInfoList = new ArrayList<NewTaskSubTaskInfo>();
-		
+
 		((StackLayout)this.getLayout()).topControl = newTaskGeneralInfo;
 		this.layout();
 	}
-	
+
 	public void showGeneralInfo() {
 		((StackLayout)this.getLayout()).topControl = newTaskGeneralInfo;
 		this.layout();
 		newTaskComposite.getNewTaskFooter().setRemoveSubTaskButtonVisible(false);
 	}
-	
+
 	public void showSubTaskInfo(int subTaskIndex) {
 		if (subTaskIndex < newTaskSubTaskInfoList.size()) {
 			((StackLayout)this.getLayout()).topControl = newTaskSubTaskInfoList.get(subTaskIndex);
@@ -64,29 +64,50 @@ public class NewTaskDetailsContainer extends Composite implements RumUpdatableVi
 		this.layout();
 		newTaskComposite.getNewTaskFooter().setRemoveSubTaskButtonVisible(true);
 	}
-	
+
 	public NewTaskComposite getNewTaskComposite() {
 		return newTaskComposite;
 	}
-	
+
 	public NewTaskGeneralInfo getNewTaskGeneralInfo() {
 		return newTaskGeneralInfo;
 	}
-	
+
 	public List<NewTaskSubTaskInfo> getNewTaskSubTaskInfoList() {
 		return newTaskSubTaskInfoList;
 	}
-	
+
 	public List<UserFile> getUserFiles() {
 		return userFiles;
 	}
 
 	@Override
 	public void controllerUpdateNotify(ControllerUpdateType updateType, Object updatedEntity) {
-		//Handle updates
+		if (this.userFiles != null && updatedEntity instanceof UserFile) {
+			UserFile userFile = (UserFile) updatedEntity;
+			if (userFile.getProject().getId()==newTaskComposite.getProjectTabFolder().getProject().getId()) {
+				switch (updateType) {
+				case CREATE:
+					userFiles.add(userFile);
+					break;
+				case MODIFIY:
+					for (int i = 0; i < userFiles.size(); i++) {
+						if (userFile.getId()==userFiles.get(i).getId()) {
+							userFiles.remove(userFiles.get(i));
+							userFiles.add(i, userFile);
+						}
+					}
+				case DELETE:
+					userFiles.remove(userFile);
+					break;
+				default:
+					break;
+				}
+			}
+		}
 		Activator.getLogger().info("Update recived (TODO)");
 	}
-	
+
 	@Override
 	public void dispose() {
 		rumController.unregisterView(this, ControllerEntityType.PROJECT);
